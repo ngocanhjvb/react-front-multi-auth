d6396126176e236ca60a99a139331d37b76102e0
NgocAnh <ngoc.anh@jvb-corp.com>
2020-05-06 00:12:51 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min'
+import 'bootstrap/dist/js/bootstrap.min'
+
+
+ReactDOM.render(
+    <App/>
+    ,
+    document.getElementById('root')
+);
